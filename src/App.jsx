fda63c18a0ea2e571f1f440809473f0a14ab6e45
Ivan <103539074+IvanYedro01@users.noreply.github.com>
2024-11-12 13:58:39 -0300

    trabeje en la modificacion del carrito de productos

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@ import { NavBar } from "./components/NavBar"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from "./context/CartContext"
+import Cart from "./components/Cart/Cart"
 
 function App() {
 
   return (
     <div className="container-app">
       <BrowserRouter>
+       <CartProvider>
         <NavBar />
 
         <Routes>
           <Route path="/" element={ <ItemListContainer saludo= {"Bienvenidos a Nexus Barber"}/>} />
           <Route path="/category/:idCategory" element={<ItemListContainer saludo={"Nexus Barber "}/>} ></Route>
           <Route path="/detail/:idProduct" element={<ItemDetailContainer/>} ></Route>
-        
+          <Route path="/cart" element={<Cart />}/>
+
+
         </Routes>
-        
+        </CartProvider>
       </BrowserRouter>
     </div>
   )
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,13 +1,18 @@
 import iconCart from "../assets/cartWidget.png"
-
-
+import { useContext } from "react"
+import { CartContext } from "../context/CartContext"
+import { Link } from "react-router-dom"
 
 const CartWidget = () => {
+
+  const {totalQuantity} = useContext(CartContext)
+
   return (
-    <div style= {{display: 'flex'}}>
+  
+    <Link  style= {{display: 'flex'}} to="/cart">
         <img src={iconCart} width={50} alt="" />
-        <p style= {{color: 'white', display: 'flex', alignItems: 'center', margin: '10px'}}>1</p>
-    </div>
+        <p style= {{color: 'white', display: 'flex', alignItems: 'center', margin: '10px'}}>{totalQuantity()}</p>
+    </Link>
   )
 }
 
